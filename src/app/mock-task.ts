28ec7d92203a1cb7186c96051f9d12025aa2c086
Syd Bautista <sydbautista07@gmail.com>
2022-07-12 22:51:33 +0800

    Upload files

---
+++ src/app/mock-task.ts
@@ -0,0 +1,47 @@
+<<<<<<< HEAD
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'May 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'May 7th at 12:30pm',
+    reminder: false,
+  },
+];
+=======
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'May 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'May 7th at 12:30pm',
+    reminder: false,
+  },
+];
+>>>>>>> b9f583f262a8dd34e551990956c14423fa91522e
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+export const environment = {
+  production: true
+};
+=======
+export const environment = {
+  production: true
+};
+>>>>>>> b9f583f262a8dd34e551990956c14423fa91522e
